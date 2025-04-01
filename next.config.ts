6626cd99aaed3278e81a2120bba7529b47870da2
Yunji Kim <normaltic@gmail.com>
2025-04-01 16:38:50 +0900

    Implement using svg setting

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ["next-mdx-remote"]
+  transpilePackages: ["next-mdx-remote"],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack"
+        }
+      ]
+    });
+
+    return config;
+  }
 };
 
 export default nextConfig;
