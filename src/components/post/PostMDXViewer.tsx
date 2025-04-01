247bbcb249470c3d5e6bd4207538303d4f62737f
Yunji Kim <normaltic@gmail.com>
2025-04-01 16:42:20 +0900

    Apply dark theme to post

--- src/components/post/PostMDXViewer.tsx
+++ src/components/post/PostMDXViewer.tsx
@@ -17,7 +17,7 @@ const COMPONENTS: Exclude<MDXRemoteProps["components"], undefined> = {
 
 function PostMDXViewer({ mdxSource }: PostMDXViewerProps) {
   return (
-    <div className="prose dark:prose-invert max-w-full prose-h3:text-3xl prose-pre:my-10 prose-blockquote:my-10 prose-blockquote:py-2 prose-blockquote:[&>p]:my-0 prose-img:my-10 prose-blockquote:border-primary prose-li:marker:text-foreground">
+    <div className="prose dark:prose-invert [&>*]:linear-transition-colors prose-code:linear-transition-colors prose-strong:linear-transition-colors prose-a:linear-transition-colors prose-li:marker:linear-transition-colors max-w-full prose-h3:text-3xl prose-pre:my-10 prose-blockquote:my-10 prose-blockquote:py-2 prose-blockquote:[&>p]:my-0 prose-img:my-10 prose-blockquote:border-primary prose-li:marker:text-foreground">
       <MDXRemote {...mdxSource} components={COMPONENTS} />
     </div>
   );
