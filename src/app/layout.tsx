c896efacb8f266719b548a67d2900c927156d923
Yunji Kim <normaltic@gmail.com>
2025-07-19 17:21:35 +0900

    Fix html lang to ko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ko" suppressHydrationWarning>
       <body
         className={`${pretendard.className} bg-background text-foreground linear-transition-colors`}
       >
